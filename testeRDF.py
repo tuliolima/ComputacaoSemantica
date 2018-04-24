e5a5b0118d5abadf204708367be087133a4d0916
nunesgrf <nunesgrf@gmail.com>
2018-04-24 16:00:13 -0300

    introducao namespace e tratamento de output

--- testeRDF.py
+++ testeRDF.py
@@ -1,29 +1,26 @@
 from rdflib import Graph
 from rdflib import URIRef
 from rdflib.namespace import RDF
+from rdflib import Namespace
 
-
+# Inicializa um TAD grafo e atribui a ontologia nele.
 g = Graph()
 g.parse("engenhariaFlorestalRDF.owl")
 
-# Relações TODO: Encontrar solução mais elegante.
-localizado = URIRef('http://www.semanticweb.org/tulio/ontologies/2018/3/engenhariaFlorestal#localizado_em')
-abrange = URIRef('http://www.semanticweb.org/tulio/ontologies/2018/3/engenhariaFlorestal#abrange')
-adapta = URIRef('http://www.semanticweb.org/tulio/ontologies/2018/3/engenhariaFlorestal#se_adapta_a')
+# Maneira elegante de acessar a URI sem precisar repeti-la.
+ontology = Namespace('http://www.semanticweb.org/tulio/ontologies/2018/3/engenhariaFlorestal#')
 
 # Captura e trata a entrada do usuário.
 cidade = URIRef('http://www.semanticweb.org/tulio/ontologies/2018/3/engenhariaFlorestal#' + input("Cidade: "))
 
-# Iterate over triples in store and print them out.
-
 # Itera sobre o grafo para encontrar o estado o qual se encontra a cidade selecionada pelo usuário.
-for (s,p,o) in g.triples((cidade, localizado, None)):
+for (s,p,o) in g.triples((cidade, ontology.localizado_em, None)):
 	local = o
 
 # Itera sobre o grafo para encontrar o bioma predominante do estado.
-for (s,p,o) in g.triples((None, abrange, local)):
+for (s,p,o) in g.triples((None, ontology.abrange, local)):
 	bioma = s
 
 # Itera sobre o grafo para encontrar e imprimir plantas que se adaptam facilmente ao bioma predominante.
-for (s,p,o) in g.triples((None, adapta, bioma)):
-	print(s)
+for (s,p,o) in g.triples((None, ontology.se_adapta_a, bioma)):
+	print(s.split('#')[-1]) # Imprime só o nome da intância. # https://stackoverflow.com/questions/20785724/rdflib-remove-namespace-from-a-uriref-resource #
